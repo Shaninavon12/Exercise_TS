33068bdc7ec54674aa8ddffacc6d9a83bc4d5157
odev954 <kleinbottle175@gmail.com>
2023-06-14 17:50:18 +0300

    cr

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,10 @@
-import {query} from './index';
-import express, { Request, Response } from "express"; 
+import { query } from "./index";
+import express, { Request, Response } from "express";
 const app = express();
-const port =3000;
+const port = 3000;
 
-app.get('/',(req : Request ,res : Response)=>{
-     res.json(query());
+app.get("/", (req: Request, res: Response) => {
+	res.json(query());
 });
 
 app.listen(port, () => console.log(` port: ${port}`));
